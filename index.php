8c305c3ebd98109d450d5cdbb404848e7e4b0c12
Esteban Ortiz A. <estebanortizarauz@gmail.com>
2020-06-18 11:16:47 -0500

    Se agregó el Login y la carpeta de css para darle estilos

--- index.php
+++ index.php
@@ -7,13 +7,39 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Registrarse</title>
+        <link rel="stylesheet" type="text/css" href="css/estilos.css">
     </head>
     <body>
-        <?php
-        echo 'Hola Mundo ESTEBAN ORTIZ';
-        echo 'Hola';
-        echo 'Hola 2';
-        ?>
+        <div class="login-wrap">
+	<div class="login-html">
+		<input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Iniciar Sesión</label>
+		
+		<div class="login-form">
+			<div class="">
+				<div class="group">
+					<label for="user" class="label">Usuario</label>
+					<input id="user" type="text" class="input">
+				</div>
+				<div class="group">
+					<label for="pass" class="label">Contraseña</label>
+					<input id="pass" type="password" class="input" data-type="password">
+				</div>
+				<div class="group">
+					<input id="check" type="checkbox" class="check" checked>
+					<label for="check"><span class="icon"></span>No cerrar Sesión</label>
+				</div>
+				<div class="group">
+					<input type="submit" class="button" value="Iniciar">
+				</div>
+				<div class="hr"></div>
+				<div class="foot-lnk">
+					<a href="#forgot">Olvidaste la contraseña?</a>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+</div>
     </body>
 </html>
