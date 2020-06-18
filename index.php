5b5490075e0110b29e65ab24247e2e958ac818d1
JoelMontalvo <67113443+JoelMontalvo@users.noreply.github.com>
2020-06-18 11:25:14 -0500

    Agregar Diseño del Login

--- index.php
+++ index.php
@@ -7,14 +7,39 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Registrarse</title>
+        <link rel="stylesheet" type="text/css" href="Styles/main.css">
     </head>
     <body>
-        <?php
-        echo 'Hola Mundo ESTEBAN ORTIZ';
-        echo 'Hola';
-        echo 'Hola 2';
-        echo 'Holaaaaaaaa';
-        ?>
+        <div class="login-wrap">
+            <div class="login-html">
+                <input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Inicar Sesion</label>
+
+                <div class="login-form">
+                    <div class="">
+                        <div class="group">
+                            <label for="user" class="label">Usuario</label>
+                            <input id="user" type="text" class="input">
+                        </div>
+                        <div class="group">
+                            <label for="pass" class="label">Contraseña</label>
+                            <input id="pass" type="password" class="input" data-type="password">
+                        </div>
+                        <div class="group">
+                            <input id="check" type="checkbox" class="check" checked>
+                            <label for="check"><span class="icon"></span>No cerrar Sesión</label>
+                        </div>
+                        <div class="group">
+                            <input type="submit" class="button" value="Iniciar">
+                        </div>
+                        <div class="hr"></div>
+                        <div class="foot-lnk">
+                            <a href="#forgot">Olvidaste la contraseña?</a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
     </body>
 </html>
