083a224774f72715afddd7533d142f26ca570eb9
Mabell <Mabell@Mabe-o_o-PC>
2020-06-25 10:13:33 -0500

    CRUD_EDITORIAL

---
+++ Controlador/CRUDEditorial.php
@@ -0,0 +1,37 @@
+<?php
+
+class CRUDEditorial {
+
+    public function crearEditorial($nombre, $ubicacion, $estado) {
+        $conexion = mysqli_connect('localhost', 'root', '', 'bd_libreria');
+
+        $sql = "CALL InsertarEditorial('$nombre', '$ubicacion', '$estado')";
+
+        mysqli_query($conexion, $sql);
+    }
+
+    public function eliminarEditorial($id) {
+
+        $conexion = mysqli_connect('localhost', 'root', '', 'bd_libreria');
+
+
+        $sql = "CALL EliminarFisicoEditorial('$id')";
+
+
+        mysqli_query($conexion, $sql);
+    }
+
+    public function editarEditorial($id, $nombre, $ubicacion, $estado) {
+
+        $conexion = mysqli_connect('localhost', 'root', '', 'bd_libreria');
+
+
+        $sql = "CALL ActualizarEditorial('$id','$nombre', '$ubicacion', '$estado')";
+
+
+        mysqli_query($conexion, $sql);
+    }
+
+}
+
+?>
